dfb992754fd42b8223873b32e94ce6e69e9e0bbc
sriram <sriram.coder2004@gmail.com>
2025-04-06 19:39:52 +0530

    fix: corrected vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: "/react_sample_deploy/",
+//   plugins: [react()]
+// })
+  
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/react_sample_deploy/",
   plugins: [react()],
-  pase:"/react_sample_deploy"
 })
